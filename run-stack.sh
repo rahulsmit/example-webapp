08a01247b71bfe7a8ad7ba8e6de2baf6a08b0f6a
Kumar, Rahul <Rahul.Kumar2@nike.com>
2019-02-21 15:06:47 -0800

    Update cluster name

--- run-stack.sh
+++ run-stack.sh
@@ -8,5 +8,5 @@ aws cloudformation $ACTION \
     --parameters \
     ParameterKey=DockerImage,ParameterValue=979913100635.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-2e6ed054 \
-    ParameterKey=Cluster,ParameterValue=rahul-cluster \
+    ParameterKey=Cluster,ParameterValue=default \
     ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-1:979913100635:listener/app/production-website/15980863fdd97e19/70b8750e7e1c6033
