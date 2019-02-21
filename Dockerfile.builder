23a2990de5c22e5c2d007c377663590451b605f4
Kumar, Rahul <Rahul.Kumar2@nike.com>
2019-02-21 14:23:34 -0800

    Adding ecs deploy

--- Dockerfile.builder
+++ Dockerfile.builder
@@ -1,6 +1,9 @@
 #This is a docker builder file
+#Start from openjdk v8 docker image with basic java dev kit
 FROM openjdk:8
 RUN echo 1
+#Update that image's local packer cache and install curl
 RUN apt-get update && apt-get install  -y curl
+#use curl to fetch the build too lein for clojure
 RUN curl -o /usr/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 RUN chmod +x /usr/bin/lein
---
+++ run-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+aws cloudformation $ACTION \
+    --region us-east-1 \
+    --stack-name $STACK_NAME \
+    --template-body file://service.yaml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters \
+    ParameterKey=DockerImage,ParameterValue=979913100635.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
+    ParameterKey=VPC,ParameterValue=vpc-2e6ed054
+    ParameterKey=Cluster,ParameterValue=rahul-cluster \
+    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-1:979913100635:listener/app/production-website/15980863fdd97e19/70b8750e7e1c6033
